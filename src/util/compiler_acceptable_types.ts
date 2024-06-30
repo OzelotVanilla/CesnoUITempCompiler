7a2c02419299222680851505e66080ebf1b78123
Ozelot Vanilla <shuihenyun@qq.com>
2024-06-30 11:47:42 +0800

    struct - Type def for Cesno UI code

--- src/util/compiler_acceptable_types.ts
+++ src/util/compiler_acceptable_types.ts
@@ -1,12 +1,111 @@
+export enum AcceptableTypeName
+{
+    object = "object",
+    number = "number",
+    string = "string",
+    component = "component",
+    array = "array"
+}
+
 export namespace AcceptableType
 {
-    export class Object { }
+    export class BaseObject
+    {
+        public static readonly class_type: AcceptableTypeName = AcceptableTypeName.object
+        public get type() { return AcceptableType.Type.make({ class_name: AcceptableTypeName.object }) }
 
-    export class Number extends Object { }
+        constructor({ }: BaseObject_Args)
+        {
 
-    export class String extends Object { }
+        }
+    }
 
-    export class Component extends Object { }
+    export class Number extends BaseObject
+    {
+        public static override readonly class_type = AcceptableTypeName.number
+        public override get type() { return AcceptableType.Type.make({ class_name: AcceptableTypeName.number }) }
+    }
 
-    export class Array<EleType> extends Object { }
+    export class String extends BaseObject
+    {
+        public static override readonly class_type = AcceptableTypeName.string
+        public override get type() { return AcceptableType.Type.make({ class_name: AcceptableTypeName.string }) }
+    }
+
+    export class Component extends BaseObject
+    {
+        public static override readonly class_type = AcceptableTypeName.component;
+        public override get type() { return AcceptableType.Type.make({ class_name: AcceptableTypeName.component }) }
+    }
+
+    export class Array<EleType extends AcceptableType.BaseObject> extends BaseObject
+    {
+        public static override readonly class_type = AcceptableTypeName.array
+        public override get type()
+        {
+            return AcceptableType.Type.make({
+                class_name: AcceptableTypeName.array,
+                generics: { EleType: this.element_type }
+            })
+        }
+
+        public readonly element_type: AcceptableType.Type
+
+        public readonly elements: EleType[]
+
+        constructor({ element_type, elements = [] }: Array_Args<EleType>)
+        {
+            super({})
+            this.elements = elements
+            this.element_type = element_type
+        }
+    }
+
+    export class Type<GenericsParam extends string = string>
+    {
+        public readonly class_name: AcceptableTypeName
+        public readonly generics: Record<GenericsParam, AcceptableType.Type> | null
+
+        public [Symbol.toPrimitive](hint: "string" | "number")
+        {
+            if (hint != "string") { throw TypeError(`AcceptableType.Type can only be converted to string, not "${hint}"`) }
+
+            if (this.generics == null) { return `${this.class_name}` }
+            else
+            {
+                // 
+                let generics_string = Object.entries(this.generics).flatMap(([_, type]) => `${type}`).join(",")
+                return `${this.class_name}<${generics_string}>`
+            }
+        }
+
+        private constructor({ class_name, generics = null }: Type_Args)
+        {
+            this.class_name = class_name
+            this.generics = generics
+        }
+
+        public static make<InferredGenericsType extends (Record<string, AcceptableType.Type> | null)>(
+            { class_name, generics = null as InferredGenericsType }: { class_name: AcceptableTypeName, generics?: InferredGenericsType }
+        )
+        {
+            return new AcceptableType.Type<Extract<keyof InferredGenericsType, string>>({
+                class_name, generics
+            })
+        }
+    }
+
+    export type BaseObject_Args = {
+
+    }
+
+    export type Array_Args<EleType> = BaseObject_Args & {
+        elements?: EleType[],
+        element_type: AcceptableType.Type
+    }
+
+    export type Type_Args = {
+        class_name: AcceptableTypeName
+        generics?: Record<string, AcceptableType.Type> | undefined | null
+    }
 }
